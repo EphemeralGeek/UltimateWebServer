bcd385edae2e38d30f2913936f06855bf710d6c9
EphemeralGeek <EphemeralGeek@users.noreply.github.com>
2019-01-12 05:36:07 +0300

    Minor tweaks
    Removed some clutter and altered dependencies a little

--- UltimateServer.sh
+++ UltimateServer.sh
@@ -24,16 +24,15 @@ fi
 #*****************
 echo "127.0.0.1 $Domain" >> /etc/hosts
 echo "127.0.0.1 $MatrixSub.$Domain" >> /etc/hosts
-echo "127.0.0.1 $NextSub.$Domain" >> /etc/hosts
 echo "127.0.0.1 $FriendicaSub.$Domain" >> /etc/hosts
 mkdir /etc/nginx/$Domain
 mkdir /etc/nginx/$Domain$MatrixSub
 mkdir /etc/nginx/$Domain$FriendicaSub
-mkdir /etc/nginx/$Domain$NextSub
 #Pulling dependancies
-wget -qO - https://matrix.org/packages/debian/repo-key.asc | sudo apt-key add -
-add-apt-repository https://matrix.org/packages/debian/
-apt -y update && apt -y upgrade && apt -y install nginx certbot
+add-apt-repository ppa:certbot/certbot
+apt -y update && apt -y upgrade && apt -y install nginx certbot letsencrypt nginx software-properties-common python-certbot-nginx git curl mariadb
+
+
 #*********************************
 # Ensures the system is up to date
 #*********************************
@@ -58,49 +57,31 @@ Install_Friendica (){
 # Installs and configures Synapse
 #********************************
 Istall_Synapse (){
-    apt -y install matrix-synapse letsencrypt nginx software-properties-common
-    systemctl enable matrix-synapse.service nginx.service
+    echo "Downloading Synapse"
+    wget -qO - https://matrix.org/packages/debian/repo-key.asc | sudo apt-key add -
+    add-apt-repository https://matrix.org/packages/debian/
+    apt update
+    apt -y install matrix-synapse 
+    systemctl start matrix-synapse && systemctl enable matrix-synapse
+
     sed -i 's/enable_registration: False/enable_registration: True/g' /etc/matrix-synapse/homeserver.yaml
     systemctl restart matrix-synapse.service
     systemctl stop nginx.service
     certbot certonly --rsa-key-size 2048 --standalone --agree-tos --no-eff-email --email $Email -d $MatrixSub.$Domain
     
     systemctl start nginx.service
-    systemctl enable nginx.service}
-#**********************************
-# Installs and configures Nextcloud
-#**********************************
-Install_NextCloud (){
-    echo "Downlaoding Nextcloud..."
-    cd /etc/nginx/$NextSub.$Domain/
-    curl https://download.nextcloud.com/server/installer/setup-nextcloud.php
-    echo "Nextcloud downlaoded."
-    #nginx config appending and certbot goes here
-}
-#*********************************
-# Checks which services to install
-#*********************************
+    systemctl enable nginx.service
+    echo "Synapse downloaded..."}
+
+#***********************
+# Kicks off installation
+#***********************
 echo "Stand by to answer a few questions, and do a little configuration."
 read -n1 -r -p "Press any key to continue..." key
-clear; echo "What servers do you want to install?\n1. Nextcloud\n2. Matrix (Synapse)\n3. Friendica\n4. All"
-read -n1 -p "\nEnter your choice here (4):" install 
 
-case $install
-    1)
-      Install_NextCloud
-    2)
-      Install_Synapse
-    3)
-      Install_Friendica
-    4)
-      Install_Synapse
-      Install_Friendica
-      install_NextCloud
-    *)
-        echo "invalid input... $install"
-        exit 1
-esac
+Install_Synapse
+Install_Friendica
 
 clear; echo "Configuration complete :), wait for the server to reboot and verify functionality"
 read -n1 -r -p "Press any key to exit..." key
-reboot
+reboot 0
